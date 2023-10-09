65032252a8219af9858e2672d71c4e529ccd3353
MikeSymoDev <mike04_norcal@yahoo.de>
2023-10-09 17:22:00 +0200

    guards small example

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -9,24 +9,24 @@ import {
   Post,
   Put,
   Query,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 // import { string } from 'yargs';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
 import { NinjasService } from './ninjas.service';
+import { BeltGuard } from 'src/belt/belt.guard';
 
 @Controller('ninjas')
 export class NinjasController {
   constructor(private readonly ninjasService: NinjasService) {}
-  // GET /ninjas?type=fast
+
   @Get()
   getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
     //  const service = new NinjasService();
     return this.ninjasService.getNinjas(weapon);
   }
-
-  // GET /ninjas/:id
 
   @Get(':id')
   getOneNinja(@Param('id', ParseIntPipe) id: number) {
@@ -37,19 +37,17 @@ export class NinjasController {
     }
   }
 
-  // POST /ninjas
   @Post()
+  @UseGuards(BeltGuard)
   createNinja(@Body(new ValidationPipe()) createNinjaDto: CreateNinjaDto) {
     return this.ninjasService.createNinja(createNinjaDto);
   }
-
-  // PUT /ninjas/:id
 
   @Put(':id')
   updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
     return this.ninjasService.updateNinja(+id, updateNinjaDto);
   }
-  // DELETE /ninjas/:id
+
   @Delete(':id')
   removeNinja(@Param('id') id: string) {
     return this.ninjasService.removeNinja(+id);
