f3070ef51ca446d407e65505d9c4c8b70f3ddb12
MikeSymoDev <mike04_norcal@yahoo.de>
2023-10-09 14:46:26 +0200

    controllers setup

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,21 +1,56 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { string } from 'yargs';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
 export class NinjasController {
-  // GET /ninjas
+  // GET /ninjas?type=fast
   @Get()
-  getNinjas() {
-    return [];
+  getNinjas(@Query('type') type: string) {
+    return [{ type }];
   }
 
   // GET /ninjas/:id
 
   @Get(':id')
-  getOneNinja() {
-    return [];
+  getOneNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+
+  // POST /ninjas
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      name: createNinjaDto.name,
+    };
+  }
+
+  // PUT /ninjas/:id
+
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return {
+      id,
+      name: updateNinjaDto,
+    };
+  }
+  // DELETE /ninjas/:id
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
   }
 }
-
-// POST /ninjas
-// PUT /ninjas/:id
-// DELETE /ninjas/:id
